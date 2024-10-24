00c5c31b15e41ca84dc464afd2d6cec3f1bf084a
Chris Hirschbrich <chrishirschbrich@gmail.com>
2024-10-24 11:52:15 -0500

    re prompted

--- llama2.py
+++ llama2.py
@@ -1,9 +1,10 @@
-def greatest_common_factor(a, b):
-    # Find the greatest common divisor of a and b
-    gcd = max(a, b)
-    return gcd
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
 
-# Examples
-print(greatest_common_factor(27, 36)) # Output: 4
-print(greatest_common_factor(144, 72)) # Output: 10
-print(greatest_common_factor(543, 34)) # Output: 10
+# Test the function with some examples
+print(gcd(27, 36)) # Output: 2
+print(gcd(144, 72)) # Output: 3
+print(gcd(543, 34)) # Output: 5
